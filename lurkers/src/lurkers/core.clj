e23e643b2c323b1fe5ec32c004689a24f2f10c1c
Eliot Skinner <Someone@DontEvenTry.org>
2021-11-11 23:02:00 -0600

    added log

--- lurkers/src/lurkers/core.clj
+++ lurkers/src/lurkers/core.clj
@@ -5,52 +5,36 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
-;;Create a record and save a log message to a log file
-;;Constructor with side effects
-
-;;define a Person record
-(defrecord Person [fname lname])
-
-;;define a function to save a log message into the log.txt using spit and :append
-(defn log-entry [msg] (spit "log.txt" (apply str msg "\n") :append true))
-
-;;build the constructor which: 1) log the message; 2)create a Person
-(defn make-person [fname lname]
-  (log-entry (apply str "[log] New Person created : " lname "," fname))
-    (->Person fname lname))
-
-;;create a person
-(def person (make-person "John" "Smith"))
-
-;;print the content of the log.txt to the console
-(println  (slurp "log.txt"))
-
-
-
-
-
-
-
-
-
 
 
 (defrecord HTML [lines])
 (defrecord File-name [name])
+(defrecord NFT-record [someId])
 
-(defn create-html-file [text file-name] (spit file-name (apply str text "\n") :append true))
+(defn record-entry [msg] (spit "log.txt" (apply str msg "\n") :append true))
+(defn create-html-file [text file-name] (spit file-name (apply str text) :append true))
 
 (defn make-html [lines file-name]
   (->HTML lines)
   (->File-name file-name)
     (create-html-file lines file-name))
 
-(def html-file (make-html "<<!DOCTYPE html>
-  <html>
-  <body>
-  
-  <h1>My First Heading</h1>
-  <p>My first paragraph.</p>
-  
-  </body>
-  </html>" (apply str "lurker_" (rand-int 385792) ".html")))
+(defn make-record [someId]
+  (record-entry (apply str "[log] new file created : " someId))
+  (->NFT-record someId))
+
+(defn create-file []
+  (def randNum (rand-int 8719)) 
+  (def html
+    (make-html "<<!DOCTYPE html>
+    <html>
+    <body>
+    
+    <h1>Rutherford the Brave</h1>
+    <p>Stealtimefromthefaultyplan</p>
+    
+    </body>
+    </html>" (apply str "lurker_" randNum ".html")))
+  (make-record (str randNum)))
+
+(def nft (create-file))
